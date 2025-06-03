1ea386677f3e5ff62ed0c1d085480387da3d1f87
SimpelDream <huayujian2012@gmail.com>
2025-06-04 02:00:22 +0800

    改错

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: 'hazard-report',
-    script: 'dist/app.js',
+    script: 'app.js',
     instances: 'max',
     exec_mode: 'cluster',
     autorestart: true,
