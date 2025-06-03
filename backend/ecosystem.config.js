6fc9e8f9a807f637bf0ef2522568ae0b45ba0bfe
SimpelDream <huayujian2012@gmail.com>
2025-06-04 01:56:02 +0800

    改错

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
-    env: {
+    env_production: {
       NODE_ENV: 'production',
       PORT: 3000,
       HOST: '0.0.0.0',
